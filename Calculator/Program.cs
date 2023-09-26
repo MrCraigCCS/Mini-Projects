03c9f3b1eb4a1b76a29b0f87ae4a286f49983303
AaronLLTT <aaron@letslearnthistogether.com>
2023-09-25 21:09:11 -0700

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please give me the first number.");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Next, give me the second number.");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"You entered {num1} and {num2}. \nNow please enter a math symbol.");
+
+string symbol = Console.ReadLine();
+
+int result = 0;
+
+if (symbol == "+")
+{
+    result = num1 + num2;
+}
+else if (symbol == "-")
+{
+    result += num1 - num2;
+}
+else if (symbol == "*")
+{
+    result = num1 * num2;
+}
+else if (symbol == "/")
+{
+    result = num1 / num2; 
+}
+else
+{
+    Console.WriteLine("Incorrect symbol. Please try agin");
+}
+
+Console.WriteLine($"The answer is {result}. Have a nice day!");
---
+++ Dice Game/Program.cs
@@ -0,0 +1,53 @@
+﻿int playerRandomNum;
+int enemyRandomNum;
+
+int playerPoints = 0;
+int enemyPoints = 0;
+
+Random random = new();
+
+for (int i = 0; i < 10; ++i)
+{
+    Console.WriteLine("Press any key to roll the dice.");
+    
+    Console.ReadKey(true);
+
+    playerRandomNum = random.Next(1, 7);
+    Console.WriteLine($"You rolled a {playerRandomNum}");
+
+    Console.WriteLine(". . . . ");
+    System.Threading.Thread.Sleep(1000);
+
+    enemyRandomNum = random.Next(1, 7);
+    Console.WriteLine($"Enemy rolled a {enemyRandomNum}");
+
+    if (playerRandomNum > enemyRandomNum)
+    {
+        playerPoints++;
+        Console.WriteLine("Player wins this round!");
+    }
+    else if (playerRandomNum < enemyRandomNum)
+    {
+        enemyPoints++;
+        Console.WriteLine("Enemy wins this round.");
+    }
+    else
+    {
+        Console.WriteLine("It's a tie!");
+    }
+
+    Console.WriteLine($"The score is now\nPlayer : {playerPoints}\nEnemy : {enemyPoints}");
+}
+
+if (playerPoints > enemyPoints)
+{
+    Console.WriteLine("You win!");
+}
+else if (enemyPoints > playerPoints)
+{
+    Console.WriteLine("You've lost.");
+}
+else
+{
+    Console.WriteLine("A tie!");
+}
